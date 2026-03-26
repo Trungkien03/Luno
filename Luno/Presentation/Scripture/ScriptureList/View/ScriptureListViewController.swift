befa4b50e2309f9583869f0e18bb04724c234786
Kain Nguyen <kain.nguyen@terralogic.com>
2026-03-26 16:37:10 +0700

    feat: Implement `UITableView` in `ScriptureListViewController` using `SnapKit` and introduce `ScriptureSection`/`ScriptureItem` data models.

--- Luno/Presentation/Scripture/ScriptureList/View/ScriptureListViewController.swift
+++ Luno/Presentation/Scripture/ScriptureList/View/ScriptureListViewController.swift
@@ -6,30 +6,49 @@
 //
 
 import UIKit
+import SnapKit
 
 class ScriptureListViewController: UIViewController {
 
     private var viewModel: ScriptureListViewModel!
-    
+
+    private lazy var tableView: UITableView = {
+        let table = UITableView(frame: .zero, style: .insetGrouped)
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        return table
+    }()
+
     private enum Layout {
         static let padding: CGFloat = 16
     }
-    
+
     // MARK: - Initialization
     init(viewModel: ScriptureListViewModel!) {
         super.init(nibName: nil, bundle: nil)
         self.viewModel = viewModel
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupUI()
     }
 
+    // Configuration
+    private func setupUI() {
+        self.title = "Scriptures"
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        self.navigationItem.largeTitleDisplayMode = .always
+        view.backgroundColor = .systemBackground
+
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
 
 }
--- Luno/Presentation/Scripture/ScriptureList/ViewModel/ScriptureListViewModel.swift
+++ Luno/Presentation/Scripture/ScriptureList/ViewModel/ScriptureListViewModel.swift
@@ -8,24 +8,39 @@
 import Foundation
 import UIKit
 
-struct ScriptureListViewModelActions {
+struct ScriptureSection: Hashable, Sendable {
+    let id: UUID
+    let title: String
     
+}
+
+struct ScriptureItem: Hashable, Sendable {
+    let id = UUID()
+    let title: String
+    let description: String
+    let iconName: String
+}
+
+struct ScriptureListViewModelActions {
+
 }
 
 protocol ScriptureListViewModelInputs {
-    
+
 }
 
 protocol ScriptureListViewModelOutputs {
-    
+
 }
 
-protocol ScriptureListViewModel: ScriptureListViewModelInputs, ScriptureListViewModelOutputs {}
+protocol ScriptureListViewModel: ScriptureListViewModelInputs,
+    ScriptureListViewModelOutputs
+{}
 
 class DefaultScriptureListViewModel: ScriptureListViewModel {
 
     var actions: ScriptureListViewModelActions?
-    
+
     init(actions: ScriptureListViewModelActions? = nil) {
         self.actions = actions
     }
